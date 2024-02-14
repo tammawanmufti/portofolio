9713c962351571eac86aa982eda9f9f2d64d6b18
Tamma <dev.mufti@gmail.com>
2024-02-14 23:06:47 +0700

    update: experience

--- src/app/api/experience/data.ts
+++ src/app/api/experience/data.ts
@@ -2,10 +2,17 @@ import { Experience } from "./model";
 
 export const experiences: Experience[] = [
   {
+    title: "Freelance Software Engineer, Various Clients (Remote)",
+    description:
+      "As a Freelance Software Developer, I have had the privilege of working with a diverse range of clients, each with unique needs and visions for their projects. Specializing in mobile and desktop application development, I have utilized cutting-edge technologies such as Flutter for cross-platform app development and React with Next.js for Progressive Web App (PWA) creation.",
+    startedAt: new Date("2018-04"),
+  },
+  {
     title: "Software Developer, Slash, Singapore (Remote)",
     description:
       "Developed a real-time Chat App using Flutter, XMPP Protocol, and GraphQL. Contributed as a Flutter Developer in developing a House Mortgage App. Served as a Backend Developer for an Investment Web App. Worked as a Frontend Developer on a community-based social media app project.",
     startedAt: new Date("2021-09"),
+    endedAt: new Date("2023-12"),
   },
   {
     title:
@@ -14,11 +21,5 @@ export const experiences: Experience[] = [
       "Developed Flutter based Mobile Application that work with iOS and android. Proactive in a small, fast-paced team to work towards problem solutions while adapting processes, practices, and tools. Designed and built advanced applocations for the Android platform. Worked with APIs to connect to 3rd party APIs (like social media) and involved in the unit-test code for robustness, including edge cases, usability, and general reliability.",
     startedAt: new Date("2019-10"),
     endedAt: new Date("2021-01"),
-  },
-  {
-    title: "Freelancer Programmer/Graphic Designer, Various Clients (Remote)",
-    description:
-      "Experience creating website , mobile app, logos design, banners, invitations, video animation and graphics. Meeting with clients to discuss project requirements and progress. Handling all verbal and written communications from clients and got enhance clients satisfied with our projects",
-    startedAt: new Date("2016-04"),
   },
 ];
